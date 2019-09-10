80640519231cb09ee6159224f0ec1135e8490eb8
Philip Jung <pjung@Philips-MacBook-Pro.local>
2019-09-09 22:00:39 -0700

    Removed all text from files to start with an Ant HelloWorld file

--- src/SearchMap.java
+++ src/SearchMap.java
@@ -1,23 +1,5 @@
-import java.io.*;
+
 
 public class SearchMap {
-	String text;
-	
-	
-	public void readMap(String fileName) throws IOException {
-		File file = new File(fileName);
-		try {
-		FileReader fileRead = new FileReader(file);
-		BufferedReader br = new BufferedReader(fileRead);
-		String line;
-		while ((line = br.readLine()) != null) {
-			text = line;
-		}
-		} catch (FileNotFoundException e) {
-			
-		}
-	}
-	public String returnText() {
-		return text;
-	}
+
 }
--- src/TestSearchMap.java
+++ src/TestSearchMap.java
@@ -5,19 +5,5 @@ import java.io.IOException;
 
 public class TestSearchMap {
 	
-	/** First test method to read file successfully
-	 * 
-	 */
-	@Test
-	public void testreadfile() {
-		SearchMap testReader = new SearchMap();
-		try {
-			testReader.readMap("src/input.txt");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// test input should just have 1 in it
-		assertEquals("SearchMap should initially just return abc", "abc", testReader.returnText());
-	}
+
 }
