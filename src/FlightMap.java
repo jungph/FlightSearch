6877149980f2460c45044c60d7cc7b6c507990e9
Philip Jung <pjung@Philips-MacBook-Pro.local>
2019-09-15 12:16:41 -0700

    FlightMap is now able to count costs and keep track of path while searching for route

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -3,30 +3,42 @@ package src;
 import java.util.ArrayList;
 
 public class FlightMap {
+	public int actualCost;
+	public ArrayList<Character> actualPath;
 	public ArrayList<Edge> edges;
 	public FlightMap() {
+		actualPath = new ArrayList<Character>();
 		edges = new ArrayList<Edge>();
+	}
+	public int getActualCost() {
+		return actualCost;
 	}
 	public void addEdge(Edge e) {
 		edges.add(e);
 	}
-	public boolean pathExistsHelper(char orig, char dest, int totalWeight, ArrayList<Edge> remEdges) {
+	public boolean pathExistsHelper(char orig, char dest, int totalWeight, ArrayList<Character> path, ArrayList<Edge> remEdges) {
 		for (int i =0; i < remEdges.size(); i++) {
 			if (remEdges.get(i).orig == orig) {
+				path.add(orig);
 				if (remEdges.get(i).dest == dest) {
+					path.add(dest);
 					totalWeight += remEdges.get(i).weight;
+					
+					// set class variables before returning true
+					actualCost = totalWeight;
+					actualPath = path;
 					return true;
 				} else {
 					Edge currentEdge = remEdges.get(i);
 					int currentWeight = currentEdge.weight;
 					totalWeight += currentWeight;
 					char nextOrig = currentEdge.dest;
-					
 					remEdges.remove(i);
-					if (pathExistsHelper(nextOrig, dest, currentWeight,remEdges)) {
+					if (pathExistsHelper(nextOrig, dest, totalWeight, path,remEdges)) {
 						return true;
 					} else {
-						totalWeight -= currentWeight;
+						path.remove(actualPath.size() - 1);
+						totalWeight -= currentEdge.weight;
 						remEdges.add(i, currentEdge);
 					}
 				}
@@ -34,8 +46,10 @@ public class FlightMap {
 		}
 		return false;
 	}
-	public boolean pathExists(char orig, char dest, int totalWeight) {
+	public boolean pathExists(char orig, char dest) {
+		actualCost = 0;
+		actualPath = new ArrayList<Character>();
 		ArrayList<Edge> remainingEdges = edges;
-		return pathExistsHelper(orig, dest, totalWeight, remainingEdges);
+		return pathExistsHelper(orig, dest, actualCost, actualPath, remainingEdges);
 	}
 }
--- src/TestFlightMap.java
+++ src/TestFlightMap.java
@@ -24,8 +24,9 @@ public class TestFlightMap {
 		e = new Edge('Z', 'R', 25);
 		f.addEdge(e);
 		
-		assertTrue(f.pathExists('P', 'R', 0));
+		assertTrue(f.pathExists('P', 'R'));
 	}
+	
 	@Test
 	public void pathExists2() {
 		FlightMap f = new FlightMap();
@@ -38,7 +39,7 @@ public class TestFlightMap {
 		e = new Edge('Z', 'T', 25);
 		f.addEdge(e);
 		
-		assertTrue(f.pathExists('P', 'T', 0));
+		assertTrue(f.pathExists('P', 'T'));
 	}
 	@Test
 	public void pathExists3() {
@@ -52,7 +53,7 @@ public class TestFlightMap {
 		e = new Edge('Z', 'F', 25);
 		f.addEdge(e);
 		
-		assertTrue(f.pathExists('W', 'F', 0));
+		assertTrue(f.pathExists('W', 'F'));
 	}
 	@Test
 	public void pathExists4() {
@@ -77,7 +78,82 @@ public class TestFlightMap {
 		f.addEdge(e);
 		e = new Edge('Y', 'R', 600);
 		f.addEdge(e);
-		assertTrue(f.pathExists('P', 'Y', 0));
+		assertTrue(f.pathExists('P', 'Y'));
+	}
+	@Test public void pathCount1() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('P', 'Y');
+		assertEquals(1600 , f.getActualCost());
+	}
+	@Test public void pathCount2() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('P', 'R');
+		assertEquals(2200 , f.getActualCost());
+	}
+	@Test public void pathCount3() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('T', 'Y');
+		assertEquals(850 , f.getActualCost());
 	}
 	@Test
 	public void pathNotExists1() {
@@ -91,7 +167,7 @@ public class TestFlightMap {
 		e = new Edge('F', 'R', 25);
 		f.addEdge(e);
 		
-		assertTrue(!f.pathExists('P', 'F', 0));
+		assertTrue(!f.pathExists('P', 'F'));
 	}
 	@Test
 	public void pathNotExists2() {
@@ -99,6 +175,84 @@ public class TestFlightMap {
 		Edge e = new Edge('P', 'W', 200);
 		f.addEdge(e);
 		
-		assertTrue(!f.pathExists('X', 'F', 0));
+		assertTrue(!f.pathExists('X', 'F'));
+	}
+	@Test
+	public void pathNotExists3() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('T', 'Y');
+		assertTrue(!f.pathExists('P','Q'));
+	}
+	@Test
+	public void pathNotExists4() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('T', 'Y');
+		assertTrue(!f.pathExists('T','Q'));
+	}
+	@Test
+	public void pathNotExists5() {
+		FlightMap f = new FlightMap();
+		Edge e = new Edge('P', 'W', 200);
+		f.addEdge(e);
+		e = new Edge('P', 'R', 300);
+		f.addEdge(e);
+		e = new Edge('R', 'X', 200);
+		f.addEdge(e);
+		e = new Edge('Q', 'X', 375);
+		f.addEdge(e);
+		e = new Edge('W', 'S', 250);
+		f.addEdge(e);
+		e = new Edge('S', 'T', 300);
+		f.addEdge(e);
+		e = new Edge('T', 'W', 350);
+		f.addEdge(e);
+		e = new Edge('W', 'Y', 500);
+		f.addEdge(e);
+		e = new Edge('Y', 'Z', 450);
+		f.addEdge(e);
+		e = new Edge('Y', 'R', 600);
+		f.addEdge(e);
+		f.pathExists('T', 'Y');
+		assertTrue(!f.pathExists('R','Q'));
 	}
 }
